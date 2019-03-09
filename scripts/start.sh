1e8f7e821269f4b66ad5c0fedd2326b7cd2c4e0d
Ric Harvey <ric@digilution.io>
2019-03-09 22:39:45 +0000

    Fix xdebug always running

--- scripts/start.sh
+++ scripts/start.sh
@@ -165,6 +165,7 @@ if [[ "$ENABLE_XDEBUG" == "1" ]] ; then
         else
             echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > $XdebugFile # Note, single arrow to overwrite file.
             echo "xdebug.remote_enable=1 "  >> $XdebugFile
+            echo "remote_host=host.docker.internal" >> $XdebugFile
             echo "xdebug.remote_log=/tmp/xdebug.log"  >> $XdebugFile
             echo "xdebug.remote_autostart=false "  >> $XdebugFile # I use the xdebug chrome extension instead of using autostart
             # NOTE: xdebug.remote_host is not needed here if you set an environment variable in docker-compose like so `- XDEBUG_CONFIG=remote_host=192.168.111.27`.
