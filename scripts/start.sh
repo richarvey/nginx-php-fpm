d32a2312a94d07ec5d30e733b783129a6f1708e1
Ievgenii Kovtun <ievgenii.kovtun@veeam.com>
2023-12-21 22:18:58 +0100

    xdebug.remote_log is deprecated. xdebug.remote_autostart is deprecated, default behaviour with  xdebug.start_with_request = default  is ok

--- scripts/start.sh
+++ scripts/start.sh
@@ -176,8 +176,7 @@ if [[ "$ENABLE_XDEBUG" == "1" ]] ; then
             echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > $XdebugFile # Note, single arrow to overwrite file.
             echo "xdebug.client_host=host.docker.internal" >> $XdebugFile
             echo "xdebug.mode=debug" >> $XdebugFile
-            echo "xdebug.remote_log=/tmp/xdebug.log"  >> $XdebugFile
-            echo "xdebug.remote_autostart=false "  >> $XdebugFile # I use the xdebug chrome extension instead of using autostart
+            echo "xdebug.log=/tmp/xdebug.log"  >> $XdebugFile
             # NOTE: xdebug.remote_host is not needed here if you set an environment variable in docker-compose like so `- XDEBUG_CONFIG=remote_host=192.168.111.27`.
             #       you also need to set an env var `- PHP_IDE_CONFIG=serverName=docker`
         fi
